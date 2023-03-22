bdb038a5ea534deadf5966fbe00772a1a269c357
JidePuffy <80157178+JidePuffy@users.noreply.github.com>
2023-03-22 16:02:02 +0100

    Add files via upload

---
+++ strings_numeric_expressions.py
@@ -0,0 +1,31 @@
+# Practice converting user input to numeric data types and perform calculations.
+
+# Write a program that does the following:
+
+# Prompt the user for their age. Convert it to a number, add one to it, and tell them how old they will be on their next birthday.
+
+# Prompt the user for the number of egg cartons they have. Assume each carton holds 12 eggs, multiply their number by 12, and display 
+# the total number of eggs.
+
+# Prompt the user for a number of cookies and a number of people. Then, divide the number of cookies by the number of people to determine 
+# how many cookies each person gets.
+
+#QUESTION 1
+# age = input("Please enter you age here:")
+# user_age = int(age)
+# next_birthday = user_age + 1
+# output = (f"You'll be {next_birthday} on your next birthday")
+# print(output)
+
+#QUESTION 2
+# egg_cartons = input("How many cartons of eggs do you have?:")
+# total_eggs = int(egg_cartons)*12
+# output = (f"Total number of eggs are {total_eggs}")
+# print(output)
+
+#QUESTION 3
+# amt_cookies = input("Please enter number of cookies:")
+# num_people = input("Please enter number of people left:")
+# each_gets = float(amt_cookies)/float(num_people)
+# output = (f"Each person gets {each_gets} amount of cookie")
+# print(output)
